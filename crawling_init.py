e23cec460f9bf31784e4e4d66c47f984108cb79e
heegene <heegene@wemakeprice.com>
2021-08-01 17:07:30 +0900

    add conditional statement - 4dx checkin released

--- crawling_init.py
+++ crawling_init.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 # CGV 메인 도메인 + 예매시간표 페이지 iframe 내 자원주소(src)
-url = "http://www.cgv.co.kr/common/showtimes/iframeTheater.aspx?areacode=02&theatercode=0052&date=20210807"
+url = "http://www.cgv.co.kr/common/showtimes/iframeTheater.aspx?areacode=02&theatercode=0052&date=20210803"
 
 response = requests.get(url)
 #print(response.text);
@@ -14,6 +14,14 @@ bs = BeautifulSoup(response.text, 'html.parser')
 # div 내 info-movie select
 infolist = bs.select('div.info-movie')
 
+# 4DX 여부를 확인하기 위해 개발자도구로 확인하니 4DX는
+# <span class="forDX">4DX</span> 이런식으로 span에 클래스 부여되어 있음
+fordx = bs.select_one('span.forDX')
+
+
 # 해당 날짜에 상영중인 영화 목록 하나씩 출력 + <strong> 태그 떼고 출력
-for movie in infolist:
-    print(movie.select_one('a > strong').text.strip())
+if (fordx):
+    for movie in infolist:
+        print(movie.select_one('a > strong').text.strip())
+else:
+    print('4dx 예매가 풀리지 않았음')
