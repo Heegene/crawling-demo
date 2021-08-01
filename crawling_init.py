a116f367f16d20ea279871e654670019bbb97dc0
heegene <heegene@wemakeprice.com>
2021-08-01 16:38:26 +0900

    영화 상영목록 받아오기

--- crawling_init.py
+++ crawling_init.py
@@ -1,6 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
 
+# CGV 메인 도메인 + 예매시간표 페이지 iframe 내 자원주소(src)
+url = "http://www.cgv.co.kr/common/showtimes/iframeTheater.aspx?areacode=02&theatercode=0052&date=20210807"
 
+response = requests.get(url)
+#print(response.text);
 
+bs = BeautifulSoup(response.text, 'html.parser')
+#print(bs.select('body > div div.info-movie > a > strong '))
+#print(bs.select('div.info-movie'))
 
+infolist = bs.select('div.info-movie')
+
+for movie in infolist:
+    print(movie.select_one('a > strong').text.strip())
